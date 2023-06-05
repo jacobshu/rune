dc82cf55e2d37fe92666a83e997c1c8f62720b83
Jacob Shulenberger <jacob@builtdigital.io>
2023-06-05 17:30:29 -0500

    feat: add simple one and two character token lexing to scanner

--- interpreter/src/scanner.rs
+++ interpreter/src/scanner.rs
@@ -12,7 +12,7 @@ pub struct Scanner {
 impl Scanner {
     pub fn new(input: String) -> Self {
         Self {
-            source: input.chars().peekable(),
+            source: input.char_indices().peekable(),
             tokens: Vec::new(),
             start: 0,
             current: 0,
@@ -20,15 +20,91 @@ impl Scanner {
         }
     }
 
+    fn is_at_end(&mut self) {
+        self.current >= source.len()
+    }
+
     fn scan_tokens(&mut self) {
         while (!self.is_at_end()) {
             self.start = self.current;
-            scan_token()
+            self.scan_token()
         }
     }
 
-    fn is_at_end(&mut self) {
-        self.current >= source.len()
+    fn scan_token(&mut self) {
+        while let Some((index, char)) = self.source.next() {
+        let token = match char {
+            '(' => Token::new(TokenType::LeftParen), 
+            ')' => Token::new(TokenType::RightParen),
+            '{' => Token::new(TokenType::LeftBrace),
+            '}' => Token::new(TokenType::RightBrace),
+            ',' => Token::new(TokenType::Comma),
+            '.' => Token::new(TokenType::Dot),
+            '+' => Token::new(TokenType::Plus),
+            '-' => Token::new(TokenType::Minus),
+            ';' => Token::new(TokenType::Semicolon),
+
+        // // One to two characters
+        '=' => {
+            match self.source.next_if_eq(&(index + 1, '=')) {
+            	Some(_equals) => Token::new(TokenType::EqualEqual),
+            	None => Token::new(TokenType::Equal),
+            }
+        },
+        '!' => {
+            match self.source.next_if_eq(&(index + 1, '=')) {
+            	Some(_equals) => Token::new(TokenType::BangEqual),
+            	None => Token::new(TokenType::Bang)
+            }
+        },
+        '>' => {
+            match self.source.next_if_eq(&(index + 1, '=')) {
+                Some(_equals) => Token::new(TokenType::GreaterEqual),
+                None => Token::new(TokenType::Greater)
+            }
+        },
+        '<' => {
+            match self.source.next_if_eq(&(index + 1, '=')) {
+                Some(_equals) => Token::new(TokenType::LessEqual),
+                None => Token::new(TokenType::Less)
+            }
+        },
+        '*' => {
+            match self.source.next_if_eq(&(index + 1, '*')) {
+                Some(_equals) => Token::new(TokenType::StarStar),
+                None => Token::new(TokenType::Star)
+            }
+        },
+                
+        // Slash,
+
+        // // Literals
+        // Identifier,
+        // String,
+        // Number,
+
+        // // Keywords
+        // And,
+        // Class,
+        // Else,
+        // False,
+        // Fun,
+        // For,
+        // If,
+        // Nil,
+        // Or,
+        // Print,
+        // Return,
+        // Super,
+        // This,
+        // True,
+        // Var,
+        // While,
+
+        // EOF,
+        _ => Token::Invalid(format!("{}", ch)),
+        };
+        tokens.push(token);
     }
 }
 
